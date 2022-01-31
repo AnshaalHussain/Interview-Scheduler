618474055558b3ae50c1ed7bbc360a633520d188
Anshaal Hussain <anshaalhussain@gmail.com>
2022-01-30 23:57:51 -0500

    Updates

---
+++ precache-manifest.b137ec6833be1a8a21650732a91d33fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec52f3c31b96c04e4f7b8388bfb8c5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c98406be09d980e2705",
+    "url": "/static/css/2.36fd44a6.chunk.css"
+  },
+  {
+    "revision": "5ccf48b1fe46327d12b1",
+    "url": "/static/css/main.57fe71e7.chunk.css"
+  },
+  {
+    "revision": "2c98406be09d980e2705",
+    "url": "/static/js/2.fc987655.chunk.js"
+  },
+  {
+    "revision": "5ccf48b1fe46327d12b1",
+    "url": "/static/js/main.ec7a6312.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
